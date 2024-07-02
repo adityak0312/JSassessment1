d2cc37255d3bd699132489f8a229cf69334a06a9
adityak0312 <149163173+adityak0312@users.noreply.github.com>
2024-07-02 21:36:41 +0530

    MetajavaScript.js

--- JStest.js
+++ JStest.js
@@ -9,41 +9,46 @@ Assessment Requirements
 */
 
 // create a variable to hold your NFT's
-const NFTs=[];
+const  NFTs = []
 // this function will take in some values as parameters, create an
 // NFT object using the parameters passed to it for its metadata, 
 // and store it in the variable above.
-function mintNFT (_name,_eyeColor,_shirtType,_bling) {
-     const NFT={
-        "name":_name,
-        "eyeColor":_eyeColor,
-        "shirtType":_shirtType,
-        "blibg":_bling
-     }
-     NFTs.push(NFT);
-     console.log("Minted: "+_name);
+function mintNFT ( _name , _eyeColor , _shirtType , _bling){
+const NFT = {
+"name" : _name,
+"eyeColor" : _eyeColor ,
+"shirtType": _shirtType,
+"bling" : _bling
+}
+NFTs. push(NFT);
+console.log( "Minted: " + _name );
 }
 
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
 function listNFTs () {
-    for(let i=0;i<NFTs.length;i++){
-        console.log("\nID: \t\t"+(i+1));
-        console.log("Name: \t\t"+NFTs[i].name);
-        console.log("eyeColor: \t"+NFTs[i].eyeColor);
-        console.log("shirtType: "+NFTs[i].shirtType);
-        console.log("Bling: \t\t"+NFTs[i].bling);
+    for (let i = 0; i < NFTs.length; i++) {
+        
+        console.log("\n Name : \t\t" + NFTs[i].name);
+        console.log("\n eyeColor: \t\t" + NFTs[i].eyeColor);
+        console.log("\n shirtType: \t\t" + NFTs[i].shirtTypes);
+        console.log("\n bling: \t\t" + NFTs[i].bling);
     }
+
 }
 
 // print the total number of NFTs we have minted to the console
 function getTotalSupply() {
-    console.log("\n"+NFTs.length);
+
+    console.log("\n" + NFTs.length);
+
 }
 
 // call your functions below this line
-mintNFT("Bob","Blue","Hoodie","Gold Chain");
-mintNFT("tim","Blue","Hoodie","Gold Chain");
-mintNFT("adi","Blue","Hoodie","Gold Chain");
-listNFTs();
+mintNFT("Ambesh", "Blue", "Hoodle" ,"Gold Chain");
+mintNFT("Parth", "Blue", "Hoodle" ,"Gold Chain");
+mintNFT("Aviral", "Blue", "Hoodle" ,"Gold Chain");
+
+listNFTs(); 
+
 getTotalSupply();
